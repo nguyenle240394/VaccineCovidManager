98deaa8f25bc13cc8df49c87b9d1788c9bf33fa3
Nguyen Le <nguyenle.httdn@gmail.com>
2022-08-05 12:07:34 +0700

    done create xuat vaccine

--- src/VaccineCovidManager.Web/Pages/ChiTietXuats/CreateModal.cshtml.cs
+++ src/VaccineCovidManager.Web/Pages/ChiTietXuats/CreateModal.cshtml.cs
@@ -5,13 +5,15 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Threading.Tasks;
 using VaccineCovidManager.ChiTietXuats;
 using VaccineCovidManager.VaccineCovids;
+using Volo.Abp;
 using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Form;
 
 namespace VaccineCovidManager.Web.Pages.ChiTietXuats
 {
-    public class CreateModalModel : PageModel
+    public class CreateModalModel : VaccineCovidManagerPageModel
     {
         private readonly ChiTietXuatAppService _chiTietXuatAppService;
         private readonly VaccineAppService _vaccineAppService;
@@ -42,6 +44,22 @@ namespace VaccineCovidManager.Web.Pages.ChiTietXuats
                 .ToList();
         }
 
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var vaccineDto = await _vaccineAppService.FindVaccineById(ChiTietXuat.VaccineID);
+            var createVaccineDto = new CreateUpdateVaccineDto();
+            var mess = "Số vaccine " + '"' +vaccineDto.TenVaccine +'"'+ " trong kho còn " + vaccineDto.SoLuongTonKho + " (liều) không đủ để xuất";
+            if (vaccineDto.SoLuongTonKho < ChiTietXuat.SoLuongXuat)
+            {
+                throw new UserFriendlyException(L[mess]);
+            }
+            createVaccineDto.TenVaccine = vaccineDto.TenVaccine;
+            createVaccineDto.SoLuongTonKho = vaccineDto.SoLuongTonKho - ChiTietXuat.SoLuongXuat;
+            await _vaccineAppService.UpdateAsync(vaccineDto.Id, createVaccineDto);
+            var upDateChiTiet = ObjectMapper.Map<CreateChiTietXuatViewModal, CreateUpdateChiTietXuatDto>(ChiTietXuat);
+            await _chiTietXuatAppService.CreateAsync(upDateChiTiet);
+            return NoContent();
+        }
         public class CreateChiTietXuatViewModal
         {
             [SelectItems(nameof(Vaccines))]
